1376f0649fcf85a3cc84176ccf414bd3569fd728
Saahil <saahilattud@gmail.com>
2024-12-13 21:05:26 -0500

    fix: any button atp

--- src/events/handle_btn.ts
+++ src/events/handle_btn.ts
@@ -1,8 +1,9 @@
 import { ModifiedApp } from "../util";
 
 export default function (app: ModifiedApp) {
-  app.action("handle_btn", async (par) => {
-    const { ack, body, client } = par;
+  app.action("*", async (par) => {
+      const { ack, body, client } = par;
+      console.log(`#button`)
     //@ts-ignore
     // if (state.values.action === "leave") {
     await client.conversations.leave({ channel: par.body.channel?.id! });
